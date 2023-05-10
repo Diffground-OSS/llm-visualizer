49d82a274438974b4dbefaca515741cbc1bca3c0
Radi Cho <radicho123@gmail.com>
2023-05-10 23:38:33 +0300

    Automate generation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import Graph from './componenets/Graph';
@@ -7,11 +8,23 @@ import './App.css';
 const theme = createTheme();
 
 function App() {
+  let [animation, setAnimation] = useState(false);
+  let [showHistory, setShowHistory] = useState(false);
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <NavBar />
-        <Graph />
+        <NavBar
+          animation={animation}
+          setAnimation={setAnimation}
+          showHistory={showHistory}
+          setShowHistory={setShowHistory}
+        />
+        <Graph
+          animation={animation}
+          showHistory={showHistory}
+          setShowHistory={setShowHistory}
+        />
       </div>
     </ThemeProvider>
   );
