12c7a42f0765ca50a594e6ce2f3f17df24b3b118
Radi Cho <radicho123@gmail.com>
2023-05-10 23:38:39 +0300

    Add constants.

---
+++ client/src/constants/index.js
@@ -0,0 +1,12 @@
+export const UTTERANCES = [
+  "Book me a hotel in Paris for May 10.",
+  "I am looking for cheap hotels.",
+  "No",
+  "Great! Book it.",
+  "No, thank you.",
+  null, // CONVERSATION BREAK
+  "Can you book me a restaurant in Plovdiv?",
+  "Today at noon.",
+  "Then try for today at 18:00.",
+  "Thank you!"
+]
